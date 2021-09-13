19a1b5244ec00d638f3b9bf2254264587aa6cf2e
Column01 <colinandress999@gmail.com>
2021-09-13 15:20:48 -0400

    Threaded API handler and auto UI reloading

--- tracker.py
+++ tracker.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import time
+import threading
 from datetime import datetime
 from tkinter import Frame, Label, Menu, Tk, font, messagebox
 
@@ -17,8 +18,6 @@ else:
 
 class ApexLegendsTracker(Frame):
     def __init__(self, master):
-        self.api_base = "https://api.mozambiquehe.re/"
-
         Frame.__init__(self, master)
         self.master = master
         self.master.title("Apex Legends Tracker")
@@ -48,9 +47,22 @@ class ApexLegendsTracker(Frame):
 
         # Start clock display
         self.update_clock()
-
         self.main_frame = None
-        self.load()
+
+        # Starts a thread to gather API data every 30 seconds with a 0.25 second delay between requests
+        self.api_thread = APIDataFetcher(self.username, self.platform, self.token, 0.1, 10.0)
+        self.api_thread.start()
+        
+        # Wait until the API thread polls once
+        while self.api_thread.polling:
+            continue
+        # Loads the data into the UI
+        self.init_window()
+
+        # Starts a task to reload the data every second
+        self.master.after(1000, self.load)
+
+        self.api_thread.username = "TESTING 123"
     
     def update_clock(self):
         self.clock.configure(text=time.strftime('%I:%M:%S %p'))
@@ -75,26 +87,8 @@ class ApexLegendsTracker(Frame):
             messagebox.showerror("Error", "No username was found in the settings file!")
         if self.platform is None:
             messagebox.showerror("Error", "No platform was found in the settings file!")
-        
-        self.params = f"?version=5&platform={self.platform}&player={self.username}&auth={self.token}"
 
-        self.made_requests = 0
-
-    def get_data(self, endpoint, params="", sleep=0):
-        """ Returns data from the unofficial apex legends API.
-
-        Paramaters:
-            endpoint (str) - The API endpoint to get data from
-            params (Optional[str]) - An optional URL encoded list of params to add to the query. Example: "&limit=10"
-            sleep (Optional[int, float]) - An optional amount of time to sleep if you are doing multiple requests. Useful to prevent rate limiting.
-        """
-        resp = requests.get(self.api_base + endpoint + self.params + params)
-        self.made_requests += 1
-        if sleep > 0:
-            time.sleep(sleep)
-        return resp.json()
-
-    def display_info(self):
+    def init_window(self):
         if self.main_frame is None:
             # Make main window frame
             self.main_frame = Frame(self.master)
@@ -107,47 +101,92 @@ class ApexLegendsTracker(Frame):
             self.main_frame.configure(bg=self.background_color)
             self.main_frame.pack(fill="both", expand=1)
 
-        _username = self.player_data["global"]["name"]
-        _level = self.player_data["global"]["level"]
-
-
         user_frame = Frame(self.main_frame)
         user_frame.configure(bg=self.background_color)
         user_frame.pack(fill="x")
-        user = Label(user_frame, text=f"User Info:", font=self.label_font, bg=self.background_color, fg=self.label_color)
-        user.pack(padx=5, pady=5, side="left")
-        level = Label(user_frame, text=f"{_username} (Level {_level})", font=self.font, bg=self.background_color, fg=self.value_color)
-        level.pack(side="left")
-
-        _cur_map = self.map_data["battle_royale"]["current"]["map"]
-        _cur_change = self.map_data["battle_royale"]["current"]["end"]
-        _next_map= self.map_data["battle_royale"]["next"]["map"]
-        _next_change = self.map_data["battle_royale"]["next"]["end"]
-
-        _cur_time = datetime.fromtimestamp(_cur_change).strftime("%I:%M %p")
-
-        _next_time = datetime.fromtimestamp(_next_change).strftime("%I:%M %p")
+        user_info_label = Label(user_frame, text=f"User Info:", font=self.label_font, bg=self.background_color, fg=self.label_color)
+        user_info_label .pack(padx=5, pady=5, side="left")
+        self.user_label = Label(user_frame, text=f"{self.api_thread.username} (Level {self.api_thread.level})", font=self.font, bg=self.background_color, fg=self.value_color)
+        self.user_label.pack(side="left")
 
         cur_map_frame = Frame(self.main_frame)
         cur_map_frame.configure(bg=self.background_color)
         cur_map_frame.pack(fill="x")
-        cur_map = Label(cur_map_frame, text="Current Map:", font=self.label_font, bg=self.background_color, fg=self.label_color)
-        cur_map.pack(padx=5, pady=5, side="left")
-        cur_map_name = Label(cur_map_frame, text=f"{_cur_map} (until {_cur_time})", font=self.font, bg=self.background_color, fg=self.value_color)
-        cur_map_name.pack(side="left")
+        cur_map_label = Label(cur_map_frame, text="Current Map:", font=self.label_font, bg=self.background_color, fg=self.label_color)
+        cur_map_label.pack(padx=5, pady=5, side="left")
+        self.cur_map_name_label = Label(cur_map_frame, text=f"{self.api_thread.cur_map} (until {self.api_thread.cur_map_end})", font=self.font, bg=self.background_color, fg=self.value_color)
+        self.cur_map_name_label.pack(side="left")
 
         next_map_frame = Frame(self.main_frame)
         next_map_frame.configure(bg=self.background_color)
         next_map_frame.pack(fill="x")
-        next_map = Label(next_map_frame, text=f"Next Map:", font=self.label_font, bg=self.background_color, fg=self.label_color)
-        next_map.pack(padx=5, pady=5, side="left")
-        next_map_name = Label(next_map_frame, text=f"{_next_map} (until {_next_time})", font=self.font, bg=self.background_color, fg=self.value_color)
-        next_map_name.pack(side="left")
+        next_map_label = Label(next_map_frame, text=f"Next Map:", font=self.label_font, bg=self.background_color, fg=self.label_color)
+        next_map_label.pack(padx=5, pady=5, side="left")
+        self.next_map_name_label = Label(next_map_frame, text=f"{self.api_thread.next_map} (until {self.api_thread.next_map_end})", font=self.font, bg=self.background_color, fg=self.value_color)
+        self.next_map_name_label.pack(side="left")
 
     def load(self):
-        self.player_data = self.get_data("bridge")
-        self.map_data = self.get_data("maprotation")
-        self.display_info()
+        # Not polling data, update the UI
+        if not self.api_thread.polling:
+            # Update username and Level
+            self.user_label.configure(text=f"{self.api_thread.username} (Level {self.api_thread.level})")
+
+            # Update current and next map info
+            self.cur_map_name_label.configure(text=f"{self.api_thread.cur_map} (until {self.api_thread.cur_map_end})")
+            self.next_map_name_label.configure(text=f"{self.api_thread.next_map} (until {self.api_thread.next_map_end})")
+
+        self.master.after(1000, self.load)
+
+
+class APIDataFetcher(threading.Thread):
+    def __init__(self, username, platform, token, request_delay, poll_interval):
+        threading.Thread.__init__(self)
+        self.api_base = "https://api.mozambiquehe.re/"
+        self.delay = request_delay
+        self.poll_interval = poll_interval
+        self.username = username
+        self.platform = platform
+        self.token = token
+
+        self.params = f"?version=5&platform={self.platform}&player={self.username}&auth={self.token}"
+
+        self.running = False
+        self.polling = False
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.polling = True
+
+            """ Player data """
+            self.player_data = self.get_data("bridge")
+            # Username and Level
+            self.username = self.player_data["global"]["name"]
+            self.level = self.player_data["global"]["level"]
+
+            """ Map data """
+            self.map_data = self.get_data("maprotation")
+            # Current Map
+            self.cur_map = self.map_data["battle_royale"]["current"]["map"]
+            self.cur_map_end = datetime.fromtimestamp(self.map_data["battle_royale"]["current"]["end"]).strftime("%I:%M %p")
+            # Next Map
+            self.next_map = self.map_data["battle_royale"]["next"]["map"]
+            self.next_map_end = datetime.fromtimestamp(self.map_data["battle_royale"]["next"]["end"]).strftime("%I:%M %p")
+
+            """ Finish poll"""
+            self.polling = False
+            time.sleep(self.poll_interval)
+
+    def get_data(self, endpoint, params=""):
+        """ Returns data from the unofficial apex legends API.
+
+        Paramaters:
+            endpoint (str) - The API endpoint to get data from
+            params (Optional[str]) - An optional URL encoded list of params to add to the query. Example: "&limit=10"
+        """
+        resp = requests.get(self.api_base + endpoint + self.params + params)
+        time.sleep(self.delay)
+        return resp.json()
 
 
 if __name__ == "__main__":
