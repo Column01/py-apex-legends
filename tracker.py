001284e96611664200c98a77688e5cb5eb18e875
Column01 <colinandress999@gmail.com>
2021-09-13 12:19:59 -0400

    Add basic display of info for maps and user info

--- tracker.py
+++ tracker.py
@@ -1,7 +1,8 @@
 import json
 import os
-from tkinter import Button, Frame, Label, Tk, font, Menu, messagebox
 import sys
+import time
+from tkinter import Frame, Label, Menu, Tk, font, messagebox
 
 import requests
 
@@ -21,14 +22,20 @@ class ApexLegendsTracker(Frame):
         self.master = master
         self.master.title("Apex Legends Tracker")
 
+        # Some constants for use later
+        self.font = font.Font(family="Helvetica Neue", size=14)
+        self.background_color = "#333333"
+        self.btn_background_color = "#655F5F"
+        self.text_color = "#FFFFFF"
+
         self.settings_file = os.path.join(__location__, "settings.json")
         self.load_settings()
 
         menu_bar = Menu(self.master)
-        menu_bar.add_command(label="Reload", command=lambda: print("test"))
+        menu_bar.add_command(label="Reload", command=self.load)
         self.master.config(menu=menu_bar)
-        self.get_data("bridge")
-        self.display_info()
+        self.main_frame = None
+        self.load()
     
     def load_settings(self):
         try:
@@ -52,16 +59,66 @@ class ApexLegendsTracker(Frame):
         
         self.params = f"?version=5&platform={self.platform}&player={self.username}&auth={self.token}"
 
-    def get_data(self, endpoint, params=""):
-        """ Gets data from the unofficial apex legends API
+        self.made_requests = 0
+
+    def get_data(self, endpoint, params="", sleep=0):
+        """ Returns data from the unofficial apex legends API.
 
         Paramaters:
             endpoint (str) - The API endpoint to get data from
             params (Optional[str]) - An optional URL encoded list of params to add to the query. Example: "&limit=10"
-
+            sleep (Optional[int, float]) - An optional amount of time to sleep if you are doing multiple requests. Useful to prevent rate limiting.
         """
         resp = requests.get(self.api_base + endpoint + self.params + params)
-        self.data = resp.json()
+        self.made_requests += 1
+        if sleep > 0:
+            time.sleep(sleep)
+        return resp.json()
+
+    def display_info(self):
+        if self.main_frame is None:
+            # Make main window frame
+            self.main_frame = Frame(self.master)
+            self.main_frame.configure(bg=self.background_color)
+            self.main_frame.pack(fill="both", expand=1)
+        else:
+            # Main frame exists so we are reloading. Destroy it and recreate it
+            self.main_frame.destroy()
+            self.main_frame = Frame(self.master)
+            self.main_frame.configure(bg=self.background_color)
+            self.main_frame.pack(fill="both", expand=1)
+
+        _username = self.player_data["global"]["name"]
+        _level = self.player_data["global"]["level"]
+
+
+        user_frame = Frame(self.main_frame)
+        user_frame.configure(bg=self.background_color)
+        user_frame.pack(fill="x")
+        user = Label(user_frame, text=f"{_username} (Level {_level})", font=self.font, bg=self.background_color, fg=self.text_color)
+        user.pack(padx=5, pady=5, side="left")
+
+        _cur_map = self.map_data["battle_royale"]["current"]["map"]
+        _cur_change = self.map_data["battle_royale"]["current"]["readableDate_end"].split(" ")[0]
+        _next_map= self.map_data["battle_royale"]["next"]["map"]
+        _next_change = self.map_data["battle_royale"]["next"]["readableDate_end"].split(" ")[0]
+
+        cur_map_frame = Frame(self.main_frame)
+        cur_map_frame.configure(bg=self.background_color)
+        cur_map_frame.pack(fill="x")
+        cur_map = Label(cur_map_frame, text=f"Current BR map: {_cur_map}\nChanges at: {_cur_change}", font=self.font, bg=self.background_color, fg=self.text_color)
+        cur_map.pack(padx=5, pady=5, side="left")
+
+        next_map_frame = Frame(self.main_frame)
+        next_map_frame.configure(bg=self.background_color)
+        next_map_frame.pack(fill="x")
+        next_map = Label(next_map_frame, text=f"Next BR map: {_next_map}\nChanges at: {_next_change}", font=self.font, bg=self.background_color, fg=self.text_color)
+        next_map.pack(padx=5, pady=5, side="left")
+
+    def load(self):
+        self.player_data = self.get_data("bridge")
+        self.map_data = self.get_data("maprotation")
+        self.display_info()
 
 
 if __name__ == "__main__":
